9db247cfb6eb5f071c1be56d95501dc2c784534d
LuisUSU <a01469880@aggies.usu.edu>
2026-06-07 15:34:05 +0000

    update2

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -2,5 +2,9 @@
 stockNums = open("/workspaces/data_3500_homework/hw4/TSLA.txt")
 lines = stockNums.readlines()
 stockPrice = []
+fiveDayAve = []
+
 for line in lines:
-    stockPrice.append(float(line))
+    stockPrice = [float(line) for line in lines]
+    fiveDayAve = [float(line) for line in lines[5]]
+print(fiveDayAve)
